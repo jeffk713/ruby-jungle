dc739986f830bcdd3181f120deea5d6f0c67ee58
jeff <jeffk713@gmail.com>
2022-01-05 22:02:56 -0800

    admin/dashboard refactor

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
-    @product_count = Product.all.map(&:quantity).inject(0, :+)
+    @product_count = Product.all.count
     @category_count = Category.all.count
   end
 end
