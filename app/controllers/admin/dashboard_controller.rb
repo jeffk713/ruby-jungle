278a589ac521277a5bc95ecf86b1b3fa1f6a642c
jeff <jeffk713@gmail.com>
2022-01-05 16:39:32 -0800

    implement dashboard counts and admin security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.all.map(&:quantity).inject(0, :+)
+    @category_count = Category.all.count
   end
 end
